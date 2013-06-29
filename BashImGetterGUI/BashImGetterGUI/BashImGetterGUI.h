7fabe3f14a473c600a613d49ab67b7b510934ccb
Benjamin <v-engr@ya.ru>
2013-06-30 01:05:07 +0400

    first prototype

---
+++ BashImGetterGUI/BashImGetterGUI/BashImGetterGUI.h
@@ -0,0 +1,32 @@
+
+// BashImGetterGUI.h : главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить stdafx.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CBiGGUIApp:
+// О реализации данного класса см. BashImGetterGUI.cpp
+//
+
+class CBiGGUIApp : public CWinApp
+{
+public:
+	CBiGGUIApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CBiGGUIApp theApp;
